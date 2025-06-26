5679cd6a9f85ac392456c6e5da84b836251a6d90
Oscar <oscarriusaki@gmail.com>
2025-06-26 01:11:47 -0400

    feat: se agrego configuraciones iniciales al main

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import dotenv from 'dotenv';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
+dotenv.config();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn'],
+  });
+
+  const configService = app.get(ConfigService);
+
+  app.enableCors({
+    origin: true,
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
+    credentials: true,
+  });
+
+  app.setGlobalPrefix(configService.get('PATH_SUBDOMAIN') || 'api');
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  const port = configService.get<string>('PORT') || '3000';
+  await app.listen(port);
+  console.log(`conectado en el servidor ${port}`);
+}
+
+void bootstrap();
